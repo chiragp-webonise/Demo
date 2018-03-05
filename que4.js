781cb29ebff877155c460dda2067b918be02380d
chiragp-webonise <36664565+chiragp-webonise@users.noreply.github.com>
2018-03-05 21:29:58 +0530

    Update que4.js

--- que4.js
+++ que4.js
@@ -1,10 +1,12 @@
+var s1,s2,s3;
 function insert() {
 
             var name = document.getElementById("Text1").value;
             var email = document.getElementById("Text2").value;
-            
+	    var timeStamp = Math.floor(Date.now() / 1000);
+	    users={};
+	    usersDetails=[];            
 	    var reg1 = new RegExp('^[a-z0-9]+$');
-	     //var reg2 = new RegExp('/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/');
     
 		if(reg1.test(name) && /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test((email)) )
 		{	    
@@ -12,18 +14,24 @@ function insert() {
 			// Check browser support
 			if (typeof(Storage) !== "undefined") {
     			// Store
-    				sessionStorage.setItem("name", name);
-				sessionStorage.setItem("email", email);
-				var timeStamp = Math.floor(Date.now() / 1000);
-				sessionStorage.setItem("timestamp",timeStamp);
-				/*
-				sessionStorage.name=name;
-				sessionStorage.email= email;
-				var timeStamp = Math.floor(Date.now() / 1000);
-				sessionStorage.timestamp=timeStamp;*/
+    				
+				users.names=name;
+				users.emails=email;
+				users.timeStamps=timeStamp;
+				usersDetails.push(users);
+
+				if(sessionStorage.users === undefined){
+					usersDetails.push(users);
+					sessionStorage.users = JSON.stringify(usersDetails);
+				}else{
+					var usersDetails = JSON.parse(sessionStorage.users);
+					usersDetails.push(users);
+					sessionStorage.users = JSON.stringify(usersDetails);
+				}
+
 				document.getElementById("p1").innerHTML = "Record inserted";    			}
     			 else {
-   			 document.getElementById("p1").innerHTML = "Sorry, your browser does 				not support Web Storage...";
+   			 document.getElementById("p1").innerHTML = "Sorry, your browser does not support Web Storage...";
 			}
 		}
 		else
@@ -31,17 +39,47 @@ function insert() {
 			document.getElementById("p1").innerHTML = "Enter correct name and email...";
 		}
         }
-		 function show() {
+function show() {
 		 
-		/*document.getElementById("names").innerHTML = document.getElementById("names").innerHTML + sessionStorage.name;	
-		document.getElementById("emails").innerHTML = document.getElementById("emails").innerHTML +sessionStorage.email;     
-		document.getElementById("timestamps").innerHTML = document.getElementById("timestamps").innerHTML + sessionStorage.timestamp;		
-        */
-			for (i = 0; i < sessionStorage.length; i++) {
-	
-			 document.getElementById("names").innerHTML=document.getElementById("names").innerHTML+sessionStorage.getItem("name");
-			 document.getElementById("emails").innerHTML=document.getElementById("emails").innerHTML+sessionStorage.getItem("email");
-			 document.getElementById("timestamps").innerHTML=document.getElementById("timestamps").innerHTML+sessionStorage.getItem("timestamp") +"<br>";
-			}
+			
+			var usersDetails = JSON.parse(sessionStorage.users);	
+			var columnCount = usersDetails.length;
+			
+			
+			    //Create a HTML Table element.
+			    var table = document.createElement("TABLE");
+		            table.border = "1";
+			//Add the header row.
+			    var row = table.insertRow(-1);
+
+				var headerCell = document.createElement("TH");
+				headerCell.innerHTML ="NAME";
+				row.appendChild(headerCell);
+				var headerCell = document.createElement("TH");
+				headerCell.innerHTML ="EMAILS";
+				row.appendChild(headerCell);
+				var headerCell = document.createElement("TH");
+				headerCell.innerHTML ="TIMESTAMPS";
+				row.appendChild(headerCell);
+			   
+				    //Add the data rows.
+					for (var k=0; k< 3; k++) {
+						var userRecord=usersDetails[k];
+						//var columnCount =userRecord[0].length;
+					    
+						    var cell = row.insertCell(-1);
+						    cell.innerHTML = userRecord.names;
+					            var cell1 = row.insertCell(-1);
+						    cell1.innerHTML = userRecord.emails;
+						    var cell2 = row.insertCell(-1);
+						    cell2.innerHTML = userRecord.timeStamps+"</br>";			
+						
+					}
+					
+ 			
+			var dvTable = document.getElementById("table");
+    			dvTable.innerHTML = "";
+   			 dvTable.appendChild(table);
+			
 			
 		}
